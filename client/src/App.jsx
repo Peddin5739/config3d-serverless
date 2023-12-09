35bc30b9715f984b203851e37da4fb76a862f39c
Naveen Kumar Peddi <peddin5739@uhcl.edu>
2023-12-08 21:50:37 -0600

    Disabled Back Button After submission

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,45 @@
-import "./App.css";
-import React, { Suspense, useEffect, useState, useRef } from "react";
+import React, { Suspense, useState } from "react";
+import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import "./App.css";
 import Colors from "./sharingcolors.jsx";
-import ShoeModel from "./ShoeModel";
-import Customize from "./customize";
+import ShoeModel from "./ShoeModel.jsx";
+import Customize from "./customize.jsx";
 import { Timer } from "./Callserver.jsx";
+import Exit from "./Exit.jsx";
+
 function App() {
+  const navigate = useNavigate();
   const [color, setColor] = useState("black");
   const [mesh, setMesh] = useState("shoe_1");
+  const [surveyCompleted, setSurveyCompleted] = useState(false);
+
   Timer({ color, mesh });
+
+  const handleSubmit = () => {
+    setSurveyCompleted(true);
+    navigate("/exit");
+  };
+
+  if (surveyCompleted) {
+    return <Exit />;
+  }
+
   return (
     <Colors.Provider value={{ color, setColor, mesh, setMesh }}>
       <div className="container">
         <div className="component left">
           <Customize />
-          <button
-            onClick={() => {
-              alert("Submit???!! You Cannot enter the survey");
-            }}
-            className="Submit-button"
-          >
-            Sumit
+          <button onClick={handleSubmit} className="Submit-button">
+            Submit
           </button>
         </div>
-
         <div className="component right">
           <Canvas shadows>
-            {/* Camera Setup */}
             <PerspectiveCamera makeDefault position={[0, 2, 5]} />
             <OrbitControls />
-
-            {/* Lighting */}
-            {/* Ambient Light */}
             <ambientLight intensity={0.3} />
-
-            {/* Directional Light */}
             <directionalLight
               position={[2, 4, 5]}
               intensity={1}
@@ -48,19 +52,13 @@ function App() {
               shadow-camera-top={10}
               shadow-camera-bottom={-10}
             />
-
-            {/* Point Light */}
             <pointLight position={[2, 2, -3]} intensity={0.5} />
-
-            {/* Hemisphere Light */}
             <hemisphereLight
-              skyColor={"#ffffff"} // bright sky color
-              groundColor={"#000000"} // dim ground color
+              skyColor={"#ffffff"}
+              groundColor={"#000000"}
               intensity={0.3}
               position={[0, 50, 0]}
             />
-
-            {/* Shoe Model */}
             <Suspense fallback={null}>
               <ShoeModel />
             </Suspense>
@@ -71,4 +69,15 @@ function App() {
   );
 }
 
-export default App;
+function AppWrapper() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/exit" element={<Exit />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppWrapper;
